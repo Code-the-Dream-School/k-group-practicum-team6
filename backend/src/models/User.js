27b3d6cd10c65a198e0830320df1c21a49caac10
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-12-18 16:20:48 -0500

    feat(models): create User schema and export User model

---
+++ backend/src/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email address is required"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+      index: true,
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+    },
+
+    passwordHash: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["student", "admin"],
+      default: "student",
+      required: true,
+    },
+  },
+
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
