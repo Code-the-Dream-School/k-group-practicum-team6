f5fda0a0d2d4bdd61b512b59b529445f5ecc3dab
Brittany Price <bprice0496@gmail.com>
2026-01-10 18:54:15 -0500

    Moved header & footer to their own files. Adjusted view entries page for redundancy.

---
+++ frontend/src/Hooks/Footer.jsx
@@ -0,0 +1,19 @@
+
+export default function Footer() {
+
+    return (
+      <>
+        <div className="footer-style">
+          <select className="dropdown-style">
+            <option value="default">Sort</option>
+            <option value="Date">Date</option>
+            <option value="Subject">Subject</option>
+            <option value="Duration">Duration</option>
+            <option value="Mood">Mood</option>
+          </select>
+          <button className="btn-style">Search</button>
+          <button className="btn-style">Pagination</button>
+        </div>
+      </>
+    );
+}
--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,17 +1,12 @@
 import { useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "flowbite-react";
-import authService from "../services/authService";
-import { useNavigate } from "react-router-dom";
+import Header from "../Hooks/Header";
+import Footer from "../Hooks/Footer";
+import Entries from "./Entries";
 
 const DashBoard = () => {
   const [openModal, setOpenModal] = useState(false);
-  const navigate = useNavigate();
 
-  const handleLogout = () => {
-    authService.removeToken();
-    authService.removeUsername();
-    navigate("/"); // redirect to login page after logout
-  };
 
   const handleModal = () => {
     if (!openModal) {
@@ -29,44 +24,37 @@ const DashBoard = () => {
       <div className="master-cont">
         {/* change to `Welcome ${name}`! */}
         <h1 className="title-style">Welcome to the Dashboard</h1>
-        <div className="header-style">
-          <button className="btn-style">+ New Entry</button>
-          <button className="btn-style">View</button>
-          <button className="btn-style" onClick={handleLogout}>Log out</button>
-        </div>
+        <Header/>
         {/* placeholder entries */}
         <div>
           <div className="entry-cont-style">
-            <Button onClick={handleModal} className="btn-style">View Entries</Button>
+            <Button onClick={handleModal} className="btn-style">
+              View Entries
+            </Button>
             {/* <button className="edit-style">Edit</button>
             <button className="delete-style">Delete</button> */}
-            
           </div>
-              <Modal show={openModal} onClose={() => setOpenModal(false)}>
-                <ModalHeader>Open</ModalHeader>
-                <ModalBody>Content</ModalBody>
-                <ModalFooter >
-                  <Button className="btn-style" onClick={handleModal}>Close</Button>
-                  </ModalFooter>
-              </Modal>
+          <Modal
+            show={openModal}
+            onClose={handleModal}
+            className="flex relative justify-center items-center gap-2 text-xl bg-blue-500
+         rounded-lg m-1 mt-2 p-5 border-2 border-solid border-[black]"
+          >
+            <ModalHeader>Open</ModalHeader>
+            <ModalBody><Entries/></ModalBody>
+            <ModalFooter>
+              <Button className="btn-style" onClick={handleModal}>
+                Close
+              </Button>
+            </ModalFooter>
+          </Modal>
           {/* <div className="entry-cont-style">
             <p className="entry-title-style">Entry 2</p>
             <button className="edit-style">Edit</button>
             <button className="delete-style">Delete</button>
           </div> */}
-        </div>
-        
-        <div className="footer-style">
-          <select className="dropdown-style">
-            <option value="default">Sort</option>
-            <option value="Date">Date</option>
-            <option value="Subject">Subject</option>
-            <option value="Duration">Duration</option>
-            <option value="Mood">Mood</option>
-          </select>
-          <button className="btn-style">Search</button>
-          <button className="btn-style">Pagination</button>
-        </div>
+         </div>
+        <Footer/>
       </div>
     </>
   );
