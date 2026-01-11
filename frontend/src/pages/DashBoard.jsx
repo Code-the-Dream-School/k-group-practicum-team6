de91913b2393c96d9765b44e025e21430d18aabb
Brittany Price <bprice0496@gmail.com>
2026-01-11 18:47:02 -0500

    Created user entry modal - more tweaks coming

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "flowbite-react";
+import { Button, Modal, ModalBody, ModalFooter } from "flowbite-react";
 import Header from "../Hooks/Header";
 import Footer from "../Hooks/Footer";
 import Entries from "./Entries";
@@ -7,54 +7,65 @@ import Entries from "./Entries";
 const DashBoard = () => {
   const [openModal, setOpenModal] = useState(false);
 
-
   const handleModal = () => {
     if (!openModal) {
       setOpenModal(true);
       console.log("Modal opened!!");
-    }
-    else {
+    } else {
       setOpenModal(false);
       console.log("Modal closed!!");
     }
-  }
+  };
 
   return (
     <>
       <div className="master-cont">
         {/* change to `Welcome ${name}`! */}
         <h1 className="title-style">Welcome to the Dashboard</h1>
-        <Header/>
-        {/* placeholder entries */}
+        <Header />
+        {/* + New Entry */}
+
+        {/* existing entries */}
         <div>
           <div className="entry-cont-style">
             <Button onClick={handleModal} className="btn-style">
               View Entries
             </Button>
-            {/* <button className="edit-style">Edit</button>
-            <button className="delete-style">Delete</button> */}
           </div>
+          {/* User entries */}
           <Modal
             show={openModal}
             onClose={handleModal}
-            className="flex relative justify-center items-center gap-2 text-xl bg-blue-500
-         rounded-lg m-1 mt-2 p-5 border-2 border-solid border-[black]"
+            size="md"
+            theme={{
+              // override default bg
+              content: { base: "bg-transparent shadow-none" },
+              body: { base: "p-0 pt-0 pb-0" },
+            }}
           >
-            <ModalHeader>Open</ModalHeader>
-            <ModalBody><Entries/></ModalBody>
+            <ModalBody
+              style={{
+                paddingTop: 0,
+                paddingBottom: 0,
+                marginTop: 0,
+                padding: "0 0.25rem",
+              }}
+              className="px-1 !pt-0 !pb-0 !mt-0"
+            >
+              <Entries/>
+            </ModalBody>
             <ModalFooter>
-              <Button className="btn-style" onClick={handleModal}>
-                Close
+              <Button
+                className="btn-style absolute top-1 right-2 pt-0.25 pb-0.25 pr-1 pl-1 bg-transparent text-xl text-white rounded"
+                onClick={handleModal}
+              >
+                X
               </Button>
             </ModalFooter>
           </Modal>
-          {/* <div className="entry-cont-style">
-            <p className="entry-title-style">Entry 2</p>
-            <button className="edit-style">Edit</button>
-            <button className="delete-style">Delete</button>
-          </div> */}
-         </div>
-        <Footer/>
+
+        </div>
+        <Footer />
       </div>
     </>
   );
--- frontend/src/pages/Entries.jsx
+++ frontend/src/pages/Entries.jsx
@@ -1,15 +1,17 @@
 const Entries = () => {
+  // add navigation
+  // handler for selecting entries
+  // another handler(s) for editing and deleting entries
   return (
     <>
-      <div className="master-cont">
-         {/* Change to `${}'s entries` */}
-        <h1 className="title-style">User Entries</h1>
-       
+      <div className="bg-blue-500 rounded-lg p-1 m-1 border-2 border-solid border-[black]">
+        {/* Change to `${}'s entries` */}
+        <h1 className="user-title-style">User Entries</h1>
+
         {/* placeholder entries */}
-        <div
-          className="bg-blue-500 rounded-lg m-1 mt-2 p-5 
-          border-2 border-solid border-[black]">
-          <div className="entry-cont-style">
+        <div>
+          <div className="user-entry-cont-style">
+            {/* change to have each entry and edit and delete buttons */}
             <button className="btn-style">Entry 1</button>
             <button className="btn-style">Entry 2</button>
           </div>
