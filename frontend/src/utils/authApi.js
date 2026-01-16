708227398fd06af09a4900ae3c85bb85f28bcea6
DanyloHet <danylohet@gmail.com>
2026-01-16 02:13:32 -0500

    api test

--- frontend/src/utils/authApi.js
+++ frontend/src/utils/authApi.js
@@ -1,28 +1,76 @@
-const fakeUser = {
-  email: 'test@test.com',
-  password: '123456',
-  name: 'TestUser',
-  token: 'mock-jwt-token',
-};
+// const fakeUser = {
+//   email: 'test@test.com',
+//   password: '123456',
+//   name: 'TestUser',
+//   token: 'mock-jwt-token',
+// };
 
+// const authApi = {
+//   async login({ email, password }) {
+//     await new Promise((res) => setTimeout(res, 500)); // mock request
 
+//     if (email === fakeUser.email && password === fakeUser.password) {
+//       return {
+//         token: fakeUser.token,
+//         name: fakeUser.name,
+//       };
+//     }
+
+//     throw new Error('Invalid email or password');
+//   },
+
+//   async register() {
+//     await new Promise((res) => setTimeout(res, 500));
+//     return { message: 'Registered successfully' };
+//   },
+// };
+
+// export default authApi;
+import { API_URL } from "../config/api";
 const authApi = {
   async login({ email, password }) {
-    await new Promise((res) => setTimeout(res, 500)); // mock request
-
-    if (email === fakeUser.email && password === fakeUser.password) {
-      return {
-        token: fakeUser.token,
-        name: fakeUser.name,
-      };
+    const res = await fetch(`${API_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // REQUIRED
+      body: JSON.stringify({ email, password }),
+    });
+    console.log(res);
+    if (!res.ok) {
+      throw new Error("Invalid email or password");
     }
 
-    throw new Error('Invalid email or password');
+    const userData = await res.json();
+    return userData;
   },
 
-  async register() {
-    await new Promise((res) => setTimeout(res, 500));
-    return { message: 'Registered successfully' };
+  async register(data) {
+    const res = await fetch(`${API_URL}/auth/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(data),
+    });
+
+    return res.json();
+  },
+
+  async getCurrentUser() {
+    const res = await fetch("http://localhost:4000/me", {
+      credentials: "include",
+    });
+
+    if (!res.ok) return null;
+    return res.json();
+  },
+
+  async logout() {
+    await fetch("http://localhost:4000/auth/logout", {
+      method: "POST",
+      credentials: "include",
+    });
   },
 };
 
