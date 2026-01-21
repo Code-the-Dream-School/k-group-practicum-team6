49394bc19049f837465f25218cce43da7065fab6
Mike McDonald <mikemcdonaldcs@gmail.com>
2026-01-21 13:05:29 -0500

    Add fetch logic for getAllEntries and createEntry

--- frontend/src/utils/entryApi.js
+++ frontend/src/utils/entryApi.js
@@ -2,23 +2,46 @@ import { API_URL } from "../config/api";
 
 const entryApi = {
   async getAllEntries() {
-    const res = await fetch(`${API_URL}/entries`, {});
+    const res = await fetch(`${API_URL}/entries`, {
+      method: "GET",
+      credentials: "include",
+    });
   },
 
-  async createEntry() {
-    const res = await fetch(`${API_URL}/entries`, {});
+  async createEntry(data) {
+    const res = await fetch(`${API_URL}/entries`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(data),
+    });
+
+    const entryData = await res.json();
+    console.log(entryData);
+    return entryData;
   },
 
   async getEntry(id) {
-    const res = await fetch(`${API_URL}/entries/${id}`, {});
+    const res = await fetch(`${API_URL}/entries/${id}`, {
+      method: "GET",
+      credentials: "include",
+    });
   },
 
   async updateEntry(id) {
-    const res = await fetch(`${API_URL}/entries/${id}`, {});
+    const res = await fetch(`${API_URL}/entries/${id}`, {
+      method: "PATCH",
+      credentials: "include",
+    });
   },
 
   async deleteEntry(id) {
-    const res = await fetch(`${API_URL}/entries/${id}`, {});
+    const res = await fetch(`${API_URL}/entries/${id}`, {
+      method: "DELETE",
+      credentials: "include",
+    });
   },
 };
 
