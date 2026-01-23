c54117246c95e4b40063a450cad293d46c02fa3d
Brittany Price <bprice0496@gmail.com>
2026-01-23 01:54:27 -0500

    Setting up provider

--- frontend/src/contexts/EntryContext.jsx
+++ frontend/src/contexts/EntryContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { API_URL } from "./config/api.js";
+import { UserProvider } from "./UserContext";
 /*
     For BrainLog, the context should own:
 
@@ -24,6 +25,26 @@ const EntryContext = createContext();
 export const EntryProvider = ({ children }) => {
     const [entry, setEntry] = useState(null);
     const [loading, setLoading] = useState(true);
-
+    const userAuth = useContext(UserProvider);
+    
     //check for authentication on mount (page load/refresh)
+    useEffect(() => {
+        const checkEntries = async () => {
+            // call UserProvider to authenticate user before updating entries
+            try {
+                setLoading(true);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+    },[]);
+    return (
+        <>
+            <EntryContext.Provider value={{entry, loading}}>
+                {children}
+            </EntryContext.Provider>
+        </>
+    )
 }
