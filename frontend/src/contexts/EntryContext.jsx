e3b6bfbefaf228945139f3d86237339fd0559591
Brittany Price <bprice0496@gmail.com>
2026-01-22 19:49:06 -0500

    Laying out how entry context should work

---
+++ frontend/src/contexts/EntryContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+import { API_URL } from "./config/api.js";
+/*
+    For BrainLog, the context should own:
+
+Core data
+
+entries
+
+the array of all study entries currently loaded
+
+pagination info
+
+current page
+
+total pages (or total count)
+
+Status data
+
+loading state
+
+“Are entries being fetched?”
+
+error state
+
+“Did something fail?”
+
+These are global concerns — dashboard, list, stats, pagination all care.
+*/
+//create context
+const EntryContext = createContext();
+
+// context provider
+export const EntryProvider = ({ children }) => {
+    const [entry, setEntry] = useState(null);
+    const [loading, setLoading] = useState(true);
+}
