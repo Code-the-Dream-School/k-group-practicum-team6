b277439c87117d98fe358b59a89dc72eddcda775
Irina Khameeva <irina.khameeva@gmail.com>
2026-01-30 15:53:42 -0500

    analytics/stat

---
+++ backend/src/controllers/analytics.js
@@ -0,0 +1,64 @@
+const Entry = require('../models/Entry');
+const { StatusCodes } = require('http-status-codes');
+
+const getDashboardStats = async (req, res) => {
+  const userId = req.user.userId;
+
+// get all entries for the user
+  const entries = await Entry.find({ createdBy: userId });
+
+  // if no entries, return zeros
+   if (entries.length === 0) {
+    return res.status(StatusCodes.OK).json({
+        timeSpent: { hours: 0, minutes: 0 },
+        averageFocus: 0,
+        overallMood: null,
+    }); 
+   }
+
+   // Time Spent
+   let totalMinutes = 0;
+
+    entries.forEach((entry) => {    
+        totalMinutes += entry.duration;
+    });
+
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+
+    // Average Focus
+    let focusSum = 0;
+
+    entries.forEach((entry) => {    
+        focusSum += entry.focus;
+    });
+
+    const averageFocus = Math.round(focusSum / entries.length);
+
+    // Overall Mood
+    const moodCount = {};
+
+    entries.forEach((entry) => {    
+        const mood = entry.mood;
+        moodCount[mood] = (moodCount[mood] || 0) + 1;
+    });
+
+    let overallMood = null;
+    let maxCount = 0;
+
+    for (const mood in moodCount) {
+        if (moodCount[mood] > maxCount) {
+            maxCount = moodCount[mood];
+            overallMood = mood;
+        }
+    }
+    res.status(StatusCodes.OK).json({
+        timeSpent: { hours, minutes },
+        averageFocus,
+        overallMood,
+    });
+};
+
+module.exports = {
+  getDashboardStats,
+};
