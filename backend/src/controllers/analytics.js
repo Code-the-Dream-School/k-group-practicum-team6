879230770ec7bc7f68507304fcac025066431e52
Irina Khameeva <irina.khameeva@gmail.com>
2026-01-31 14:50:53 -0500

    Backend for analytics data(stats)

--- backend/src/controllers/analytics.js
+++ backend/src/controllers/analytics.js
@@ -1,62 +1,59 @@
-const Entry = require('../models/Entry');
-const { StatusCodes } = require('http-status-codes');
+const Entry = require("../models/Entry");
+const { StatusCodes } = require("http-status-codes");
 
 const getDashboardStats = async (req, res) => {
   const userId = req.user.userId;
 
-// get all entries for the user
+  // get all entries for the user
   const entries = await Entry.find({ createdBy: userId });
 
   // if no entries, return zeros
-   if (entries.length === 0) {
+  if (entries.length === 0) {
     return res.status(StatusCodes.OK).json({
-        timeSpent: { hours: 0, minutes: 0 },
-        averageFocus: 0,
-        overallMood: null,
-    }); 
-   }
-
-   // Time Spent
-   let totalMinutes = 0;
-
-    entries.forEach((entry) => {    
-        totalMinutes += entry.duration;
+      timeSpent: { hours: 0, minutes: 0 },
+      averageFocus: 0,
+      overallMood: null,
     });
+  }
 
-    const hours = Math.floor(totalMinutes / 60);
-    const minutes = totalMinutes % 60;
+  // Time Spent
+  let totalMinutes = 0;
 
-    // Average Focus
-    let focusSum = 0;
+  entries.forEach((entry) => {
+    totalMinutes += entry.duration;
+  });
 
-    entries.forEach((entry) => {    
-        focusSum += entry.focus;
-    });
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
 
-    const averageFocus = Math.round(focusSum / entries.length);
+  // Average Focus
+  let focusSum = 0;
 
-    // Overall Mood
-    const moodCount = {};
+  entries.forEach((entry) => {
+    focusSum += entry.focus;
+  });
 
-    entries.forEach((entry) => {    
-        const mood = entry.mood;
-        moodCount[mood] = (moodCount[mood] || 0) + 1;
-    });
+  const averageFocus = Math.round(focusSum / entries.length);
 
-    let overallMood = null;
-    let maxCount = 0;
+  // Overall Mood
+  const moodCount = {};
+  let overallMood = null;
+  let maxCount = 0;
 
-    for (const mood in moodCount) {
-        if (moodCount[mood] > maxCount) {
-            maxCount = moodCount[mood];
-            overallMood = mood;
-        }
+  entries.forEach(({ mood }) => {
+    moodCount[mood] = (moodCount[mood] || 0) + 1;
+
+    if (moodCount[mood] > maxCount) {
+      maxCount = moodCount[mood];
+      overallMood = mood;
     }
-    res.status(StatusCodes.OK).json({
-        timeSpent: { hours, minutes },
-        averageFocus,
-        overallMood,
-    });
+  });
+
+  res.status(StatusCodes.OK).json({
+    timeSpent: { hours, minutes },
+    averageFocus,
+    overallMood,
+  });
 };
 
 module.exports = {
