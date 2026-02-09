2f6a85365022c13968bc5f003c51a7ad0b3d32ad
Mike McDonald <mikemcdonaldcs@gmail.com>
2026-02-09 18:57:49 -0500

    style: update app level color scheme for light/dark modes

--- frontend/src/components/styles/layout/Layout.jsx
+++ frontend/src/components/styles/layout/Layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Layout({ children }) {
   return (
     <>
-      <div className="min-h-screen bg-gray-100 text-gray-900">
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
         <div className="mx-auto max-w-6xl px-6 py-8">{children}</div>
       </div>
     </>
