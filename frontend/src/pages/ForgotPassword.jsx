9bd0958964ffbee11d2e8d215ca5652e1f432ce3
Brittany Price <bprice0496@gmail.com>
2026-02-10 01:59:23 -0500

    Added handler for submit & email useState

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,18 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button, Card, Label, TextInput } from "flowbite-react";
+
 export default function ForgotPassword () {
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    } 
+
     return (
       <>
         <Card className="text-white text-xl flex justify-center items-center">
-          <form className="flex max-w-md flex-col gap-4">
+          <form onSubmit={handleSubmit} className="flex max-w-md flex-col gap-4">
             <h2 className="text-3xl font-semibold text-center text-blue-600 
             mb-6">Forgot Password</h2>
             <div>
               <Label className="">Email:</Label>
-              <TextInput className="" type="email" placeholder="Email" required/>
+              <TextInput onChange={(event) => setEmail(event.target.value)} 
+              type="email" value={email} placeholder="Email" required/>
             </div>
             <Button className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md
-            hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 cursor-pointer" type="submit">Submit</Button>
+            hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 cursor-pointer" 
+            type="submit">Submit</Button>
           </form>
         </Card>
       </>
