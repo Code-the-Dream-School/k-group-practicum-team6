d638ed59e70e0e2d3213570f25a3f5b2f63e69cd
Irina Khameeva <irina.khameeva@gmail.com>
2026-02-16 08:52:20 -0500

    Removed the commented out code

--- frontend/src/components/EntryForm.jsx
+++ frontend/src/components/EntryForm.jsx
@@ -89,9 +89,6 @@ const EntryForm = ({ initialData, persistEntry, onClose }) => {
               <option value="3">3 hrs</option>
             </Select>
 
-            {/* {errors.hours && (
-              <p className="text-red-500 text-sm">{errors.hours.message}</p>
-            )} */}
           </div>
 
           <div className="flex flex-col gap-1">
@@ -107,9 +104,6 @@ const EntryForm = ({ initialData, persistEntry, onClose }) => {
               <option value="45">45 mins</option>
             </Select>
 
-            {/* {errors.minutes && (
-              <p className="text-red-500 text-sm">{errors.minutes.message}</p>
-            )} */}
           </div>
         </div>
         {/*Error messages*/}
